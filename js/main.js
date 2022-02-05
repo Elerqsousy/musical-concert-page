a15a3a22b699706eb92f73195ee0ff8dbf6ef70b
Elerqsousy <mahmoud.eleresousy@gmail.com>
2022-02-05 14:44:55 +0200

    Separate menu js from main js file

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const navMenueItems = document.querySelector(".nav-menu-items");
-const hamIcon = document.querySelector(".ham");
-const closeIcon = document.querySelector(".close-icon");
-const navList = document.querySelectorAll(".nav-menu-item");
-
-function openMenu() {
-  navMenueItems.classList.remove("display-none");
-}
-
-function closeMenu() {
-  navMenueItems.classList.add("display-none");
-}
-
-hamIcon.addEventListener("click", openMenu);
-closeIcon.addEventListener("click", closeMenu);
-navList.forEach((e) => e.addEventListener("click", closeMenu));
-
 const artists = [
   {
     imgSrc: "./src/imgs/billie-eilish.jpg",
